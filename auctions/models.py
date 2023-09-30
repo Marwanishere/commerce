1bd67706a23c2ea9f5e7320338a2414b73194a05
Marwanishere <marwan.hasan.work@gmail.com>
2023-10-01 00:22:59 +0100

    max char length of bids reduced to 32 characters

--- auctions/models.py
+++ auctions/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class User(AbstractUser):
     pass
     auction_listings = models.CharField(max_length = 64),
-    bids = models.CharField(max_length = 64),
+    bids = models.CharField(max_length = 32),
     comments = models.CharField(max_length = 64),
