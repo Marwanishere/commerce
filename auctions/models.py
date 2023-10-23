caf07df99845dd667c2f53271a1fd69332ba94de
Marwanishere <marwan.hasan.work@gmail.com>
2023-10-23 13:33:18 +0100

    went through models.py and adjusted the user class, i have also made three other classes and included the relevant connections and relationships such as many to one or one to many relationships these classes include auctionlisting, bid and comment

--- auctions/models.py
+++ auctions/models.py
@@ -1,9 +1,24 @@
+from sys import maxsize
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+#The cs50 chatbot was used to help generate the User, AuctionListing,
+# Bid and Comment classes
 class User(AbstractUser):
-    pass
-    auction_listings = models.CharField(max_length = 64),
-    bids = models.CharField(max_length = 32),
-    comments = models.CharField(max_length = 64),
+    bio = models.CharField(max_length= 64*4),
+    profile_photo = models.ImageField()
+
+class AuctionListing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "auction_listings"),
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "bid" )
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "comment")
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+
