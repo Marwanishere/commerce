7b9244e7dd7d92fcaa1cc52016997857eeb69f71
Marwanishere <marwan.hasan.work@gmail.com>
2023-11-23 12:44:06 +0000

    attempting to provide an option for the maker of the bid to close the bid. the function has already been made in views.py however when summoned in the listing template it causes an error

--- auctions/views.py
+++ auctions/views.py
@@ -149,7 +149,7 @@ def listing_view(request, listing_id):
     highest = max((bid.bid_amount for bid in listing.auction_listing.all()), default=0)
     return render(request, 'auctions/listing.html', {'listing': listing, 'amount':amount, 'highest': highest})
 
-def closing_bid_view(request, listing_id, current_bids):
+def closing_bid_view(request, listing_id):
     listing = AuctionListing.objects.get(id = listing_id)
     if listing == None:
         return form.add_error('listing',"No such listing found")
